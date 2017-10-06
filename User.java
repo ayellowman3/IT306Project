9fb9d97f01ffde42e968f1fbf484d946c69a5e2f
George Huang <georgehuang@Georges-MBP.fios-router.home>
2017-10-05 23:15:03 -0400

    Edited George

--- User.java
+++ User.java
@@ -18,7 +18,43 @@ public class User {
         this.password = password;
     }
 
-    public static int getCount(){
-        return count;
-    }
+	/**
+	* Returns value of count
+	* @return
+	*/
+	public static int getCount() {
+		return count;
+	}
+
+	/**
+	* Returns value of email
+	* @return
+	*/
+	public String getEmail() {
+		return this.email;
+	}
+
+	/**
+	* Returns value of password
+	* @return
+	*/
+	public String getPassword() {
+		return this.password;
+	}
+
+	/**
+	* Sets new value of email
+	* @param
+	*/
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	* Sets new value of password
+	* @param
+	*/
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
